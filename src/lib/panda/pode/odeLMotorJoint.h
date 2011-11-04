4f4da5c449d105101d3861a974a8f30e6d241765
Ralf Kaestner <ralf.kaestner@gmail.com>
2011-11-04 11:43:16 +0000

    Fixed bugs in OdeAMotorJoint that would cause all parameters to be set on the first axis Completed implementation of OdeLMotorJoint

--- src/lib/panda/pode/odeLMotorJoint.h
+++ src/lib/panda/pode/odeLMotorJoint.h
@@ -26,13 +26,30 @@ PUBLISHED:
   INLINE void set_num_axes(int num);
   INLINE void set_axis(int anum, int rel, dReal x, dReal y, dReal z);
   INLINE void set_axis(int anum, int rel, const LVecBase3f &axis);
-  INLINE void set_param(int parameter, dReal value);
 
   INLINE int get_num_axes() const;
   INLINE LVecBase3f get_axis(int anum) const;
   MAKE_SEQ(get_axes, get_num_axes, get_axis);
-  INLINE dReal get_param(int parameter) const;
 
+  INLINE void set_param_lo_stop(int axis, dReal val);
+  INLINE void set_param_hi_stop(int axis, dReal val);
+  INLINE void set_param_vel(int axis, dReal val);
+  INLINE void set_param_f_max(int axis, dReal val);
+  INLINE void set_param_fudge_factor(int axis, dReal val);
+  INLINE void set_param_bounce(int axis, dReal val);
+  INLINE void set_param_CFM(int axis, dReal val);
+  INLINE void set_param_stop_ERP(int axis, dReal val);
+  INLINE void set_param_stop_CFM(int axis, dReal val);
+
+  INLINE dReal get_param_lo_stop(int axis) const;
+  INLINE dReal get_param_hi_stop(int axis) const;
+  INLINE dReal get_param_vel(int axis) const;
+  INLINE dReal get_param_f_max(int axis) const;
+  INLINE dReal get_param_fudge_factor(int axis) const;
+  INLINE dReal get_param_bounce(int axis) const;
+  INLINE dReal get_param_CFM(int axis) const;
+  INLINE dReal get_param_stop_ERP(int axis) const;
+  INLINE dReal get_param_stop_CFM(int axis) const;
 public:
   static TypeHandle get_class_type() {
     return _type_handle;
